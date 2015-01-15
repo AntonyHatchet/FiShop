005c29cd40d0a44202313e87e975afc4e5e6a112
shtefan <2>
2015-01-15 23:54:04 +0300

    admin panel: users

---
+++ shop/routes/admin.js
@@ -0,0 +1,17 @@
+/**
+ * Created by shtefan on 16.01.15.
+ */
+
+var db = require('../dbMagic/data.js'),
+config = require('../../config/config.js')();
+
+module.exports = {
+
+    // Get shop home page
+    getControlPanelUsers: function(req, res) {
+        // Get categories for top nav
+                // Render home page
+                res.render('control/users', {
+                });
+    }
+};
