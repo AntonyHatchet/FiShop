8dff78865e524f38b2364cabd386ab1fe17f8e02
Anton Gorshenin <anton_gorshenin@etb-lab.com>
2015-01-23 15:38:50 +0300

    Отключил  обработку showcase на главной при помощи packery

--- shop/public/js/plugins/packeryShowcase.js
+++ shop/public/js/plugins/packeryShowcase.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var $container = $('.showcase').packery({
         gutter:0,
         columnWidth: 180,
-        rowHeight: 175
-        //isInitLayout: false
+        rowHeight: 175,
+        isInitLayout: false
     });
 });
